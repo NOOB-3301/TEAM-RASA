2f5543df3e52117979f619b2161995ab70ce2139
raunit-dev <raunitjaiswal510@gmail.com>
2025-02-14 12:47:32 +0530

    modified the schema

--- server/model/course.model.js
+++ server/model/course.model.js
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
-
+import { Admin } from "./admin.model";
 
 const courseSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    price: Number,
-    imageLink: String,
-    published: Boolean
-  });
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    imageLink: { type: String },
+    published: { type: Boolean, default: false },
+    teacher: { type: mongoose.Schema.Types.ObjectId, ref: "Admin", required: true } 
+});
 
-const Course = mongoose.model("Course", courseSchema)
+const Course = mongoose.model("Course", courseSchema);
 
-export {Course}
+export { Course };
