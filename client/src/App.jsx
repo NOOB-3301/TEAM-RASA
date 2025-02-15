6f328f108e6a3cebd699f6a7143f7c420e200c24
Subhankar Rajbanshi <subhankar18r@gmail.com>
2025-02-16 01:28:36 +0530

    add: home route and page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,17 @@ import LandingPage from "./Components/LandingPage.jsx/LandingPage";
 import Login from "./Components/Auth/Login";
 import Signup from "./Components/Auth/Signup";
 import CourseList from "./Components/Courses/CourseList";
+import Home from "./Components/Home";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/signup" element={<Signup/>} />
-        <Route path="/courses" element={<CourseList/>} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/courses" element={<CourseList />} />
       </Routes>
     </Router>
   );
