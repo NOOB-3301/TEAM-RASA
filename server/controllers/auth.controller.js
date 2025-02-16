f132f9da04bc69ffbd12a43a2b83b09fa35a7767
Subhankar Rajbanshi <subhankar18r@gmail.com>
2025-02-16 11:02:57 +0530

    removed: additional data

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -125,13 +125,6 @@ const login = async (req, res) => {
         return res.status(200).json({
             message: "Login successful",
             token,
-            user: {
-                _id: user._id,
-                username: user.username,
-                email: user.email,
-                role: user.role
-            },
-            roleData
         });
 
     } catch (error) {
