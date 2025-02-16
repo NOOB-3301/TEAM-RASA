d36329d224d16d5441c32fd865d0c5f13a285ec3
Subhankar Rajbanshi <subhankar18r@gmail.com>
2025-02-16 11:02:41 +0530

    change : formatting

--- server/routes/course.routes.js
+++ server/routes/course.routes.js
@@ -1,14 +1,18 @@
-import express from 'express'
-import { createCourse, getAllCourses, getCourseByTeacher, getCourseDetails } from '../controllers/course.controller.js'
-import multer from 'multer'
+import express from "express";
+import {
+  createCourse,
+  getAllCourses,
+  getCourseByTeacher,
+  getCourseDetails,
+} from "../controllers/course.controller.js";
+import multer from "multer";
 
+const courseRouter = express.Router();
+const upload = multer({ Storage: multer.memoryStorage() });
 
-const courseRouter = express.Router()
-const upload = multer({Storage: multer.memoryStorage()})
+courseRouter.route("/createcourse").post(upload.single("image"), createCourse);
+courseRouter.route("/getallcourse").get(getAllCourses);
+courseRouter.route("/getcoursedetails").post(getCourseDetails);
+courseRouter.route("/getcoursebyeacher").post(getCourseByTeacher);
 
-courseRouter.route("/createcourse").post(upload.single('image') ,createCourse)
-courseRouter.route("/getallcourse").get(getAllCourses)
-courseRouter.route("/getcoursedetails").post(getCourseDetails)
-courseRouter.route("/getcoursebyeacher").post(getCourseByTeacher)
-
-export {courseRouter}
+export { courseRouter };
